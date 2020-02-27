6bcbbaf62defdfecc45a88ed8b42ea5d924ca1e0
Nielo Wait <nielowait@gmail.com>
2020-02-27 11:21:56 +0200

    Interface and Waypoint class (for gizmo)

---
+++ Assets/00 - Game Root/IAIAgent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vega {
+    public interface IAIAgent
+    {
+        void ModifyHealth(int value);
+        void BoostHitPoints(float t, float boost);
+        
+        float Health { get; }
+        float HitPoints { get; }        
+    }
+}
---
+++ Assets/00 - Game Root/Waypoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Waypoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            transform.position = hit.point;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.up);
+        Gizmos.DrawSphere(transform.position + Vector3.up, .2f);
+    }
+}
